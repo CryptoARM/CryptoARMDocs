18d510771ff820868bc7e04c15a88ba17ec411de
@nka1 <nka@digt.ru>
2018-12-14 12:25:12 +0300

    Version 1.1.1 Fixed support android CryptoARM

--- trusted.cryptoarmdocs/install/version.php
+++ trusted.cryptoarmdocs/install/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.1.0",
-    "VERSION_DATE" => "2018-12-14 9:00:00"
+    "VERSION" => "1.1.1",
+    "VERSION_DATE" => "2018-12-14 10:00:00"
 );
 
