ee09221e6f650f6c0d28d542c56c5007b4c802e0
vva <vva@digt.ru>
2019-03-21 10:22:10 +0300

    Version 1.2.1
    Installed mail event and template
    
    No actual changes

--- trusted.cryptoarmdocs/install/version.php
+++ trusted.cryptoarmdocs/install/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.2.0",
-    "VERSION_DATE" => "2019-03-20 10:00:00"
+    "VERSION" => "1.2.1",
+    "VERSION_DATE" => "2019-03-20 16:00:00"
 );
 
