f133f771299893f92c6d7948047de6ef88b380f3
vva <vva@digt.ru>
2019-05-28 14:13:21 +0300

    Iss #42 WorkflowDocument wrapper for documents

---
+++ trusted.cryptoarmdocs/classes/WorkflowDocument.php
@@ -0,0 +1,168 @@
+<?php
+namespace Trusted\CryptoARM\Docs;
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Bizproc\FieldType;
+use Bitrix\Main\GroupTable;
+
+if (!\CModule::IncludeModule('bizproc')) {
+    return;
+}
+
+class WorkflowDocument implements \IBPWorkflowDocument {
+
+    public static function getDocumentType($id) {
+        return 'TR_CA_DOC';
+    }
+
+    public static function getComplexDocumentType() {
+        return [TR_CA_DOCS_MODULE_ID, self::class, 'TR_CA_DOC'];
+    }
+
+    public static function getComplexDocumentId($id) {
+        return [TR_CA_DOCS_MODULE_ID, self::class, $id];
+    }
+
+    public static function CreateDocument($pid, $fields) {
+        $doc = Utils::CreateDocument($fields['file'], array_diff_key($fields, ['file' => true]));
+        return $doc->getId();
+    }
+
+    public static function DeleteDocument ($id) {
+        Database::getDocumentById($id)->getLastDocument()->remove();
+    }
+
+    public static function GetDocument($id) {
+        return Database::getDocumentById($id)->getLastDocument()->toArray();
+    }
+
+    public static function GetDocumentFields($documentType) {
+        return [
+            'id' => [
+                'Name' => 'Id',
+                'Type' => FieldType::INT,
+                'Editable' => false,
+                'Required' => true,
+            ],
+            'name' => [
+                'Name' => 'Name',
+                'Type' => FieldType::STRING,
+                'Editable' => true,
+                'Required' => true,
+            ],
+            'path' => [
+                'Name' => 'Path',
+                'Type' => FieldType::STRING,
+                'Editable' => false,
+                'Required' => true,
+            ],
+            'type' => [
+                'Name' => 'Type',
+                'Type' => FieldType::STRING,
+                'Editable' => false,
+                'Required' => true,
+            ],
+            'status' => [
+                'Name' => 'Status',
+                'Type' => FieldType::STRING,
+                'Editable' => false,
+                'Required' => true,
+            ],
+            'signers' => [
+                'Name' => 'Signers',
+                'Type' => FieldType::STRING,
+                'Editable' => false,
+                'Required' => true,
+            ],
+            'parentId' => [
+                'Name' => 'ParentId',
+                'Type' => FieldType::INT,
+                'Editable' => false,
+                'Required' => true,
+            ],
+            'childId' => [
+                'Name' => 'ChildId',
+                'Type' => FieldType::INT,
+                'Editable' => false,
+                'Required' => true,
+            ],
+        ];
+    }
+
+    public static function getDocumentAdminPage($id) {
+        $doc = Database::getDocumentById($id);
+        if (!$doc) {
+            return '';
+        }
+        $lastDocId = $doc->getLastDocument()->getId();
+        return "/bitrix/components/trusted/docs/ajax.php?command=content&id=$lastDocId";
+    }
+
+    public static function canUserOperateDocument($operation, $userId, $documentId, $arParameters = array()) {
+        return true;
+    }
+
+    public static function canUserOperateDocumentType($operation, $userId, $documentType, $arParameters = array()) {
+        // Can be used to disallow non-admins from accessing bp editor
+        return true;
+    }
+
+    public static function UpdateDocument($id, $arFields, $modifiedById = null) {
+    }
+
+    public static function PublishDocument($id) {
+        return false;
+    }
+
+    public static function UnpublishDocument($id) {
+        return false;
+    }
+
+    public static function LockDocument($id, $workflowId) {
+        return false;
+    }
+
+    public static function UnlockDocument($id, $workflowId) {
+        $doc = Database::getDocumentById($id)->getLastDocument();
+        $doc->unblock();
+        $doc->save();
+        return true;
+    }
+
+    public static function IsDocumentLocked($id, $workflowId) {
+        return Database::getDocumentById($id)->getLastDocument()->getStatus() == DOC_STATUS_BLOCKED;
+    }
+
+    public static function getAllowableOperations($documentType) {
+        return [];
+    }
+
+    public static function getAllowableUserGroups($documentType) {
+        $dbAdminGroup = GroupTable::getById(1);
+        $adminGroup = $dbAdminGroup->fetch();
+
+        return array(
+            'group_1' => $adminGroup['NAME'],
+        );
+    }
+
+    public static function getUsersFromUserGroup($group, $id) {
+        $group = strtolower($group);
+
+        $groupId = intval(str_replace('group_', '', $group));
+        if ($groupId <= 0) {
+            return array();
+        }
+
+        return \CGroup::GetGroupUser($groupId);
+    }
+
+    public static function GetDocumentForHistory($id, $historyIndex) {
+        return false;
+    }
+
+    public static function recoverDocumentFromHistory($id, $arDocument) {
+        return false;
+    }
+
+}
+
