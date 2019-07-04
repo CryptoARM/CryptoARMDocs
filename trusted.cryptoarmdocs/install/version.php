272396fcdc137c4ef929b70d0e87b4235e3b0e45
vva <vva@digt.ru>
2019-07-04 14:47:13 +0300

    Version 1.4.0

--- trusted.cryptoarmdocs/install/version.php
+++ trusted.cryptoarmdocs/install/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.3.1",
-    "VERSION_DATE" => "2019-06-07 10:00:00"
+    "VERSION" => "1.4.0",
+    "VERSION_DATE" => "2019-07-04 10:00:00"
 );
 
