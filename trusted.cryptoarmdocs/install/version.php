fcc8207df26c9d44a659c875ad62eb58ce870928
vva <vva@digt.ru>
2019-08-19 12:14:01 +0300

    Version 1.5.0

--- trusted.cryptoarmdocs/install/version.php
+++ trusted.cryptoarmdocs/install/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.4.0",
-    "VERSION_DATE" => "2019-07-04 10:00:00"
+    "VERSION" => "1.5.0",
+    "VERSION_DATE" => "2019-08-19 10:00:00"
 );
 
