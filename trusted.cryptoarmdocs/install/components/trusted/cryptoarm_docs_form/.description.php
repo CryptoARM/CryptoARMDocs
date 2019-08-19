85812360fca367e338e29f04ea63f69386f390b3
vva <vva@digt.ru>
2019-08-19 13:49:05 +0300

    Uncomment form component

--- trusted.cryptoarmdocs/install/components/trusted/cryptoarm_docs_form/.description.php
+++ trusted.cryptoarmdocs/install/components/trusted/cryptoarm_docs_form/.description.php
@@ -7,10 +7,10 @@ use Bitrix\Main\Localization\Loc;
 $arComponentDescription = [
     'NAME' => Loc::getMessage("TR_CA_DOCS_COMP_FORM_NAME"),
     'DESCRIPTION' => Loc::getMessage("TR_CA_DOCS_COMP_FORM_DESCRIPTION"),
-//    'PATH' => array(
-//        'ID' => 'CryptoARM Documents',
-//        "NAME" => Loc::getMessage("TR_CA_DOCS_COMP_FORM_PATH_NAME"),
-//    ),
+    'PATH' => array(
+        'ID' => 'CryptoARM Documents',
+        "NAME" => Loc::getMessage("TR_CA_DOCS_COMP_FORM_PATH_NAME"),
+    ),
     'CACHE_PATH' => 'Y',
 ];
 
