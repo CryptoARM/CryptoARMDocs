0827f9f4f3676d3d640103c3da8dbb0951d966eb
nka <nka@digt.ru>
2019-08-23 10:48:23 +0300

    Iss #178 Refactoring comp: Form

--- trusted.cryptoarmdocs/install/components/trusted/cryptoarm_docs_form/templates/.default/template.php
+++ trusted.cryptoarmdocs/install/components/trusted/cryptoarm_docs_form/templates/.default/template.php
@@ -3,6 +3,164 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 use Bitrix\Main\Localization\Loc;
 
+function createHTMLElement($value) {
+    ?>
+    <input type="hidden"
+           id="<?= "input_html_" . $value["ID"] ?>"
+           name="<?= "input_html_" . $value["ID"] ?>"
+           value="<?= $value["DEFAULT_VALUE"]["TEXT"] ?>"
+    />
+    <?
+}
+
+function createDateInput($value) {
+    ?>
+    <div class="trca-sf-date">
+        <input type="date"
+               id="<?= "input_date_" . $value["ID"] ?>"
+               name="<?= "input_date_" . $value["ID"] ?>"
+               value="<?= $value["DEFAULT_VALUE"] ?>"
+            <? echo $value["IS_REQUIRED"] == "Y" ? "required" : "" ?>
+        />
+    </div>
+    <?
+}
+
+function createTextInput($value) {
+    ?>
+    <div class="trca-sf-input">
+        <input type="text"
+               id="<?= "input_text_" . $value["ID"] ?>"
+               name="<?= "input_text_" . $value["ID"] ?>"
+               placeholder="<?= $value["HINT"] ?>"
+               value="<?= $value["DEFAULT_VALUE"] ?>"
+            <? echo $value["IS_REQUIRED"] == "Y" ? "required" : "" ?>
+        />
+        <div class="trca-sf-input-footer"></div>
+    </div>
+    <?
+}
+
+function createFileInput($value, $arResult) {
+    $multiple = $value["MULTIPLE"] == "Y" ? "_Y" : "";
+    ?>
+    <div id="trca-sf-upload-button-<?= $value["ID"] ?>">
+        <div class="trca-sf-upload-button-input"
+             id="<?= 'trca-sf-upload-button-input-' . $value['ID'] . '_0' ?>">
+            <div class="trca-sf-upload-input"
+                 id="<?= 'trca-sf-upload-input-' . $value['ID'] . '_0' ?>">
+                <input type="file"
+                       id="<?= "input_file_" . $value["ID"] . "_0" . $multiple ?>"
+                       name="<?= "input_file_" . $value["ID"] . "_0" . $multiple ?>"
+                       onchange="checkSizeNReadNWrite(<?= $value['ID'] . ', 0,' . "'" . $multiple . "'" . ',' . $arResult["MAX_UPLOAD_FILE_SIZE"] ?>)"
+                    <?
+                    echo $value["IS_REQUIRED"] == "Y" ? "required" : "" ?>
+                />
+                <?= Loc::getMessage("TR_CA_DOCS_COMP_FORM_INPUT_FILE"); ?>
+            </div>
+            <div class="trca-sf-upload-file-button"
+                 id="<?= 'trca-sf-upload-file-button-' . $value['ID'] . '_0' ?>">
+                <div class="trca-sf-upload-file">
+                    <div class="trca-sf-upload-file-icon">
+                        <i class="material-icons">
+                            insert_drive_file
+                        </i>
+                    </div>
+                    <div class="trca-sf-upload-file-name"
+                         id="<?= 'trca-sf-upload-file-name-' . $value['ID'] . '_0' ?>"></div>
+                    <div class="trca-sf-upload-file-remove"
+                        <?
+                        if ($value["MULTIPLE"] == "Y") {
+                            ?>
+                            onclick="removeUploadFile(<?= $value['ID'] ?>, 0)"
+                            <?
+                        } else {
+                            ?>
+                            onclick="hideUploadFile(<?= $value['ID'] ?>)"
+                            <?
+                        }
+                        ?>
+                    >
+                        <i class="material-icons">
+                            close
+                        </i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?
+}
+
+function createNumberInput($value) {
+    ?>
+    <div class="trca-sf-input-number">
+        <input type="number"
+               id="<?= "input_number_" . $value["ID"] ?>"
+               name="<?= "input_number_" . $value["ID"] ?>"
+               value="<?= $value["DEFAULT_VALUE"] ?>"
+               placeholder="<?= $value["HINT"] ?>"
+            <? echo $value["IS_REQUIRED"] == "Y" ? "required" : "" ?>
+        />
+        <div class="trca-sf-input-footer"></div>
+    </div>
+    <?
+}
+
+function createCheckBoxElement($value, $key) {
+    foreach ($value["ADDITIONAL"] as $key2 => $value2) {
+        ?>
+        <div class="trca-sf-checkbox">
+            <input type="checkbox"
+                   id="<?= "input_checkbox_" . $key . "_" . $key2 ?>"
+                   name="<?= "input_checkbox_" . $key . "_" . $key2 ?>"
+            />
+            <label for="<?= "input_checkbox_" . $key . "_" . $key2 ?>"></label>
+            <div class="trca-sf-checkbox-value">
+                <?= $value2 ?>
+            </div>
+        </div>
+        <?
+    }
+}
+
+function createSelectElement($value) {
+    ?>
+    <div class="trca-sf-selector">
+        <select
+                id="<?= "input_select_" . $value["ID"] ?>"
+                name="<?= "input_select_" . $value["ID"] ?>">
+            <?
+            foreach ($value["ADDITIONAL"] as $key2 => $value2) {
+                ?>
+                <option value="<?= $key2 ?>"><?= $value2 ?></option>
+                <?
+            }
+            ?>
+        </select>
+        <div class="trca-sf-drop-down"></div>
+    </div>
+    <?
+}
+
+function createRadioButtonElement($value) {
+    foreach ($value["ADDITIONAL"] as $key2 => $value2) {
+        ?>
+        <div class="trca-sf-radioBTN">
+            <input type="radio"
+                   id="<?= "input_radio_" . $value["ID"] ?>"
+                   name="<?= "input_radio_" . $value["ID"] ?>"
+                   value="<?= $key2 ?>"
+                <? echo $value["IS_REQUIRED"] == "Y" ? "required" : "" ?>
+            />
+            <label>
+                <?= $value2 ?>
+            </label>
+        </div>
+        <?
+    }
+}
+
 // iframe for file upload
 ?>
 <iframe id="trCaDocs__frame"
@@ -33,43 +191,18 @@ use Bitrix\Main\Localization\Loc;
                                 switch ($value["USER_TYPE"]) {
                                     case "HTML" :
                                         {
-                                            echo htmlspecialchars_decode($value["DEFAULT_VALUE"]["TEXT"]); ?>
-                                            <input type="hidden"
-                                                   id="<?= "input_html_" . $value["ID"] ?>"
-                                                   name="<?= "input_html_" . $value["ID"] ?>"
-                                                   value="<?= $value["DEFAULT_VALUE"]["TEXT"] ?>"
-                                            />
-                                            <?
+                                            echo htmlspecialchars_decode($value["DEFAULT_VALUE"]["TEXT"]);
+                                            createHTMLElement($value);
                                         }
                                         break;
                                     case "Date" :
                                         {
-                                            ?>
-                                            <div class="trca-sf-date">
-                                                <input type="date"
-                                                       id="<?= "input_date_" . $value["ID"] ?>"
-                                                       name="<?= "input_date_" . $value["ID"] ?>"
-                                                       value="<?= $value["DEFAULT_VALUE"] ?>"
-                                                    <? echo $value["IS_REQUIRED"] == "Y" ? "required" : "" ?>
-                                                />
-                                            </div>
-                                            <?
+                                            createDateInput($value);
                                         }
                                         break;
                                     default :
                                     {
-                                        ?>
-                                        <div class="trca-sf-input">
-                                            <input type="text"
-                                                   id="<?= "input_text_" . $value["ID"] ?>"
-                                                   name="<?= "input_text_" . $value["ID"] ?>"
-                                                   placeholder="<?= $value["HINT"] ?>"
-                                                   value="<?= $value["DEFAULT_VALUE"] ?>"
-                                                <? echo $value["IS_REQUIRED"] == "Y" ? "required" : "" ?>
-                                            />
-                                            <div class="trca-sf-input-footer"></div>
-                                        </div>
-                                        <?
+                                        createTextInput($value);
                                     }
                                 }
                             }
@@ -78,67 +211,9 @@ use Bitrix\Main\Localization\Loc;
                         case "N":
                             {
                                 if (stristr($value["CODE"], "DOC_FILE")) {
-                                    $multiple = $value["MULTIPLE"] == "Y" ? "_Y" : "";
-                                    ?>
-                                    <div id="trca-sf-upload-button-<?= $value["ID"] ?>">
-                                        <div class="trca-sf-upload-button-input"
-                                             id="<?= 'trca-sf-upload-button-input-' . $value['ID'] . '_0' ?>">
-                                            <div class="trca-sf-upload-input"
-                                                 id="<?= 'trca-sf-upload-input-' . $value['ID'] . '_0' ?>">
-                                                <input type="file"
-                                                       id="<?= "input_file_" . $value["ID"] . "_0" . $multiple ?>"
-                                                       name="<?= "input_file_" . $value["ID"] . "_0" . $multiple ?>"
-                                                       onchange="checkSizeNReadNWrite(<?= $value['ID'] . ', 0,' . "'" . $multiple . "'" . ',' . $arResult["MAX_UPLOAD_FILE_SIZE"] ?>)"
-                                                    <?
-                                                    echo $value["IS_REQUIRED"] == "Y" ? "required" : "" ?>
-                                                />
-                                                <?= Loc::getMessage("TR_CA_DOCS_COMP_FORM_INPUT_FILE"); ?>
-                                            </div>
-                                            <div class="trca-sf-upload-file-button"
-                                                 id="<?= 'trca-sf-upload-file-button-' . $value['ID'] . '_0' ?>">
-                                                <div class="trca-sf-upload-file">
-                                                    <div class="trca-sf-upload-file-icon">
-                                                        <i class="material-icons">
-                                                            insert_drive_file
-                                                        </i>
-                                                    </div>
-                                                    <div class="trca-sf-upload-file-name"
-                                                         id="<?= 'trca-sf-upload-file-name-' . $value['ID'] . '_0' ?>"></div>
-                                                    <div class="trca-sf-upload-file-remove"
-                                                        <?
-                                                        if ($value["MULTIPLE"] == "Y") {
-                                                            ?>
-                                                            onclick="removeUploadFile(<?= $value['ID'] ?>, 0)"
-                                                            <?
-                                                        } else {
-                                                            ?>
-                                                            onclick="hideUploadFile(<?= $value['ID'] ?>)"
-                                                            <?
-                                                        }
-                                                        ?>
-                                                    >
-                                                        <i class="material-icons">
-                                                            close
-                                                        </i>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <?
+                                    createFileInput($value, $arResult);
                                 } else {
-                                    ?>
-                                    <div class="trca-sf-input-number">
-                                        <input type="number"
-                                               id="<?= "input_number_" . $value["ID"] ?>"
-                                               name="<?= "input_number_" . $value["ID"] ?>"
-                                               value="<?= $value["DEFAULT_VALUE"] ?>"
-                                               placeholder="<?= $value["HINT"] ?>"
-                                            <? echo $value["IS_REQUIRED"] == "Y" ? "required" : "" ?>
-                                        />
-                                        <div class="trca-sf-input-footer"></div>
-                                    </div>
-                                    <?
+                                    createNumberInput($value);
                                 }
                             }
                             break;
@@ -146,61 +221,18 @@ use Bitrix\Main\Localization\Loc;
                         case "L":
                         {
                             if ($value["MULTIPLE"] == "Y") {
-                                foreach ($value["ADDITIONAL"] as $key2 => $value2) {
-                                    ?>
-                                    <div class="trca-sf-checkbox">
-                                        <input type="checkbox"
-                                               id="<?= "input_checkbox_" . $key . "_" . $key2 ?>"
-                                               name="<?= "input_checkbox_" . $key . "_" . $key2 ?>"
-                                        />
-                                        <label for="<?= "input_checkbox_" . $key . "_" . $key2 ?>"></label>
-                                        <div class="trca-sf-checkbox-value">
-                                            <?= $value2 ?>
-                                        </div>
-                                    </div>
-                                    <?
-                                }
+                                createCheckBoxElement($value, $key);
                                 break;
                             }
                             switch ($value["LIST_TYPE"]) {
                                 case "L" :
                                     {
-                                        ?>
-                                        <div class="trca-sf-selector">
-                                            <select
-                                                    id="<?= "input_select_" . $value["ID"] ?>"
-                                                    name="<?= "input_select_" . $value["ID"] ?>">
-                                                <?
-                                                foreach ($value["ADDITIONAL"] as $key2 => $value2) {
-                                                    ?>
-                                                    <option value="<?= $key2 ?>"><?= $value2 ?></option>
-                                                    <?
-                                                }
-                                                ?>
-                                            </select>
-                                            <div class="trca-sf-drop-down"></div>
-                                        </div>
-                                        <?
+                                        createSelectElement($value);
                                     }
                                     break;
-                                case
-                                "C" :
+                                case "C" :
                                     {
-                                        foreach ($value["ADDITIONAL"] as $key2 => $value2) {
-                                            ?>
-                                            <div class="trca-sf-radioBTN">
-                                                <input type="radio"
-                                                       id="<?= "input_radio_" . $value["ID"] ?>"
-                                                       name="<?= "input_radio_" . $value["ID"] ?>"
-                                                       value="<?= $key2 ?>"
-                                                    <? echo $value["IS_REQUIRED"] == "Y" ? "required" : "" ?>
-                                                />
-                                                <label>
-                                                    <?= $value2 ?>
-                                                </label>
-                                            </div>
-                                            <?
-                                        }
+                                        createRadioButtonElement($value);
                                     }
                                     break;
                             }
@@ -235,4 +267,3 @@ use Bitrix\Main\Localization\Loc;
         </div>
     </div>
 </form>
-
