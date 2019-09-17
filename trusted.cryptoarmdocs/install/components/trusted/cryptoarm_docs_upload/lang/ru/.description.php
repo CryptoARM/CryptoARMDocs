ef74ca6057f597956dc4425c733ba624bd629730
vva <vva@digt.ru>
2019-09-17 14:32:20 +0300

    Add descriptions for crm and upload

---
+++ trusted.cryptoarmdocs/install/components/trusted/cryptoarm_docs_upload/lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?php
+
+$MESS["TR_CA_DOCS_COMP_DOCS_UPLOAD"] = "Загрузка документов пользователя";
+$MESS["TR_CA_DOCS_COMP_DOCS_UPLOAD_LIST"] = "Список документов";
+$MESS["TR_CA_DOCS_COMP_DOCS_UPLOAD_DOC"] = "КриптоАРМ Документы";
