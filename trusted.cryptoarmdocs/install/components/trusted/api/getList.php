5a77262a7acd2a628ece7c8da6209d91c71275f9
nka <nka@digt.ru>
2019-12-17 10:27:14 +0300

    Iss #283 added getList API controller

---
+++ trusted.cryptoarmdocs/install/components/trusted/api/getList.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/bitrix/components/trusted/api/getUser.php";
+
+use Trusted\CryptoARM\Docs;
+use Bitrix\Main\Loader;
+use Bitrix\Main\Config\Option;
+use Bitrix\Main\ModuleManager;
+
+$coreIds = [
+    'trusted.cryptoarmdocscrp',
+    'trusted.cryptoarmdocsbusiness',
+    'trusted.cryptoarmdocsstart',
+];
+$module_id = 'not found';
+
+foreach ($coreIds as $coreId) {
+    $corePathDir = $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/" . $coreId . "/";
+    if (file_exists($corePathDir)) {
+        $module_id = $coreId;
+        break;
+    }
+}
+
+Loader::includeModule($module_id);
+
+define("NO_KEEP_STATISTIC", true);
+define("BX_STATISTIC_BUFFER_USED", false);
+define("NO_LANG_FILES", true);
+define("NOT_CHECK_PERMISSIONS", true);
+
+header('Content-Type: application/json; charset=' . LANG_CHARSET);
+
+function echoAndDie($answer) {
+    echo json_encode($answer);
+    die();
+}
+
+$userId = getUserIdByToken($_REQUEST["token"]);
+
+if ($userId["code"]) {
+    echoAndDie($userId);
+}
+
+$data = [];
+$docsList = Docs\Database::getDocumentsByUser($userId, true);
+
+foreach ($docsList->getList() as $doc) {
+    $data[] = $doc->getId();
+}
+
+$answer = [
+    "code" => 200,
+    "message" => "ok",
+    "date" => $data
+];
+
+echoAndDie($answer);
---
+++ trusted.cryptoarmdocs/install/components/trusted/api/getUser.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php";
+
+use Trusted\Id;
+use Bitrix\Main\Loader;
+use Bitrix\Main\Config\Option;
+use Bitrix\Main\ModuleManager;
+
+Loader::includeModule("trusted.id");
+
+define("NO_KEEP_STATISTIC", true);
+define("BX_STATISTIC_BUFFER_USED", false);
+define("NO_LANG_FILES", true);
+define("NOT_CHECK_PERMISSIONS", true);
+
+function getUserIdByToken($token) {
+    if (!IsModuleInstalled("trusted.id")) {
+        $answer = [
+            "code" => 800,
+            "message" => "trusted.id is not installed",
+            "data" => []
+        ];
+        return $answer;
+    }
+
+    if (!$_REQUEST["token"]) {
+        $answer = [
+            "code" => 801,
+            "message" => "token is not find",
+            "data" => []
+        ];
+        return $answer;
+    }
+
+    try {
+        $responseByToken = Id\TAuthCommand::getUserProfileByToken($_REQUEST["token"]);
+    } catch (Exception $exception) {
+        $answer = [
+            "code" => 804,
+            "message" => "something wrong",
+            "data" => []
+        ];
+        return $answer;
+    }
+
+    if (!$responseByToken["entityId"]) {
+        $answer = [
+            "code" => 802,
+            "message" => "user did not give permission",
+            "data" => []
+        ];
+        return $answer;
+    }
+
+    $userInfo = Id\TDataBaseUser::getUserById($responseByToken["entityId"]);
+    $userId = (int)$userInfo->getUserId();
+
+    if (!$userId) {
+        $answer = [
+            "code" => 803,
+            "message" => "user is not find",
+            "data" => []
+        ];
+        return $answer;
+    }
+
+    return $userId;
+}
