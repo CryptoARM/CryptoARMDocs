bc40246f8d0be3684aace828a4d2b9dbf706fbd1
nka <nka@digt.ru>
2019-12-23 13:54:12 +0300

    Iss #290 added addDoc API controller

---
+++ trusted.cryptoarmdocs/install/components/trusted/api/addDoc.php
@@ -0,0 +1,91 @@
+<?php
+
+require_once $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/bitrix/components/trusted/api/getUser.php";
+
+use Trusted\CryptoARM\Docs;
+use Bitrix\Main\Loader;
+use Bitrix\Main\Config\Option;
+use Bitrix\Main\ModuleManager;
+use Bitrix\Main\Localization\Loc;
+
+$coreIds = [
+    'trusted.cryptoarmdocscrp',
+    'trusted.cryptoarmdocsbusiness',
+    'trusted.cryptoarmdocsstart',
+];
+$module_id = 'not found';
+foreach ($coreIds as $coreId) {
+    $corePathDir = $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/" . $coreId . "/";
+    if (file_exists($corePathDir)) {
+        $module_id = $coreId;
+        break;
+    }
+}
+
+require_once $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/" . $module_id . "/lang/ru/classes/Utils.php";
+
+Loader::includeModule($module_id);
+
+define("NO_KEEP_STATISTIC", true);
+define("BX_STATISTIC_BUFFER_USED", false);
+define("NO_LANG_FILES", true);
+define("NOT_CHECK_PERMISSIONS", true);
+
+header('Content-Type: application/json; charset=' . LANG_CHARSET);
+
+function echoAndDie($answer) {
+    echo json_encode($answer);
+    die();
+}
+
+$userId = getUserIdByToken($_REQUEST["token"]);
+
+if (!$_FILES) {
+    $answer = [
+        "code" => 902,
+        "message" => "document does not exist",
+        "data" => []
+    ];
+    return $answer;
+}
+
+global $USER;
+$USER->Authorize($userId);
+
+$data = [];
+
+$uniqid = (string)uniqid();
+
+$newDocDir = $_SERVER['DOCUMENT_ROOT'] . '/' . $DOCUMENTS_DIR . '/' . $uniqid . '/';
+mkdir($newDocDir);
+
+$newDocFilename = Docs\Utils::mb_basename($_FILES["file"]['name']);
+$newDocFilename = preg_replace('/[\s]+/u', '_', $newDocFilename);
+$newDocFilename = preg_replace('/[^a-zA-Z' . Loc::getMessage("TR_CA_DOCS_CYR") . '0-9_\.-]/u', '', $newDocFilename);
+$absolutePath = $newDocDir . $newDocFilename;
+$relativePath = $DOCUMENTS_DIR . $uniqid . '/' . $newDocFilename;
+
+if (move_uploaded_file($_FILES["file"]['tmp_name'], $absolutePath)) {
+    $props = new Docs\PropertyCollection();
+    $props->add(new Docs\Property("USER", (string)$userId));
+    $doc = Docs\Utils::createDocument($relativePath, $props);
+}
+
+unset($_FILES["file"]);
+
+$USER->Logout();
+
+$answer = [
+    "code" => 200,
+    "message" => "ok",
+    "data" => [
+        $doc->getId() => [
+            "code" => 200,
+            "message" => "ok",
+            "id" => $doc->getId()
+        ]
+    ]
+];
+
+echoAndDie($answer);
