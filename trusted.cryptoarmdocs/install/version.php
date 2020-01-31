3b1c23a39b66f295ba04813b6c39a9c00ba469ba
nka <nka@digt.ru>
2020-01-31 17:15:09 +0300

    2.1.1 update version.php

--- trusted.cryptoarmdocs/install/version.php
+++ trusted.cryptoarmdocs/install/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "2.1.0",
-    "VERSION_DATE" => "2020-01-21 10:00:00"
+    "VERSION" => "2.1.1",
+    "VERSION_DATE" => "2020-01-29 10:00:00"
 );
 
