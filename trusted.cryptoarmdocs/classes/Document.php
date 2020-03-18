71d4bbd23b2893a82375c3c528df879e4943be02
nka <nka@digt.ru>
2020-03-18 18:02:57 +0300

    Iss #355 change JSON file specification and change method of content download file for new API

--- trusted.cryptoarmdocs/classes/Document.php
+++ trusted.cryptoarmdocs/classes/Document.php
@@ -886,7 +886,7 @@ class Document implements IEntity, ISave {
             "name" => $this->getName(),
             "url" => $this->getUrl(),
             "id" => $this->getId(),
-            "urlOrigDoc" => $this->getSignType() === DOC_SIGN_TYPE_DETACHED ? TR_CA_DOCS_AJAX_CONTROLLER . "?command=content&id=" . $this->originalId . "&force=true" : null,
+            "urlDetached" => $this->getSignType() === DOC_SIGN_TYPE_DETACHED ? $this->getUrl() . "&detachedSign=true" : null,
         ];
         return $a;
     }
