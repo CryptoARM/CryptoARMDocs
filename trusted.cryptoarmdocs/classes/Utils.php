fcac2be9ba49cc8ec66a3747229f876abefb8764
Николай Канюшков <nka@digt.ru>
2020-04-14 14:52:08 +0000

    Iss #363 revoke sign url in mail template

--- trusted.cryptoarmdocs/classes/Utils.php
+++ trusted.cryptoarmdocs/classes/Utils.php
@@ -603,5 +603,17 @@ class Utils
     public static function checkCurl() {
         return in_array('curl', get_loaded_extensions());
     }
+
+    /**
+     * Check if a given string is a valid UUID
+     * @param string $uuid The string to check
+     * @return bool
+     */
+    function isValidUuid($uuid) {
+        if (!is_string($uuid) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) !== 1)) {
+            return false;
+        }
+        return true;
+    }
 }
 
