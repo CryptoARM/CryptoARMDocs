1ef44dceb2e9081dd51324e992460b871d65a194
Ваше Имя <you@example.com>
2020-11-28 11:28:23 +0300

    change copy order

--- extra/deploy.sh
+++ extra/deploy.sh
@@ -5,10 +5,10 @@ reduction="start"
 
 sudo rm -rf $path_to_module
 sudo cp -R trusted.cryptoarmdocs/ $path_to_module
+sudo cp  extra/crnm.php $path_to_module
 sudo chown -R alr:www-data $path_to_module
 sudo find $path_to_module -type f -exec chmod 0664 {} \;
 sudo find $path_to_module -type d -exec chmod 2775 {} \;
-sudo cp  extra/crnm.php $path_to_module
 
 cd "/var/www/"$1"/bitrix/modules/"
 if [ -d "bitrix.eshop" ]; then
