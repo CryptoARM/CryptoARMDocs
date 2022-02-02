432316accbfd3a5c733ba73991b53ec88851c809
Александр Гаврилов <alg@digt.ru>
2022-02-03 00:56:58 +0300

    fix deploy

--- extra/deploy.sh
+++ extra/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-path_to_module="/var/www/"$1"/bitrix/modules/trusted.cryptoarmdocsbusiness/"
+path_to_module="/var/www/"$1"/bitrix/modules/trusted.cryptoarmdocsfree/"
 reduction="start"
 
 sudo rm -rf $path_to_module
@@ -9,40 +9,3 @@ sudo cp  extra/crnm.php $path_to_module
 sudo chown -R alex:www-data $path_to_module
 sudo find $path_to_module -type f -exec chmod 0664 {} \;
 sudo find $path_to_module -type d -exec chmod 2775 {} \;
-
-cd "/var/www/"$1"/bitrix/modules/"
-if [ -d "bitrix.eshop" ]; then
-  reduction="biz"
-fi
-
-cd "/var/www/"$1
-if [ -d "bizproc" ]; then
-  reduction="b24"
-fi
-
-cd $path_to_module
-
-case $reduction in
-     start)
-		  sudo sed -i -e "s;cryptoarmdocscrp;cryptoarmdocsstart;g" crnm.php
-		  sudo chmod 775 crnm.php
-		  sudo php crnm.php
-		  cd ..
-		  sudo mv trusted.cryptoarmdocsbusiness trusted.cryptoarmdocsstart
-          ;;
-     b24)
-
-		  sudo chmod 775 crnm.php
-		  sudo php crnm.php
-		  cd ..
-		  sudo mv trusted.cryptoarmdocsbusiness trusted.cryptoarmdocscrp
-          ;;
-esac
-
-
-
-
-
-
-
-
