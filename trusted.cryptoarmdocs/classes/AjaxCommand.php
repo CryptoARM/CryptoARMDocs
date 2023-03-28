d1b7bbd2fcbeedaa2b0f92c091655dd30077df77
van@digt.ru <van@digt.ru>
2023-03-28 17:54:14 +0300

    2

--- trusted.cryptoarmdocs/classes/AjaxCommand.php
+++ trusted.cryptoarmdocs/classes/AjaxCommand.php
@@ -128,7 +128,18 @@ class AjaxCommand {
 
         $license = License::getOneTimeLicense();
         $res['license'] = $license['data'];
-        
+
+        function console_log($data){ // сама функция
+            if(is_array($data) || is_object($data)){
+		        echo("<script>console.log('php_array: ".json_encode($data)."');</script>");
+	        } else {
+                echo("<script>console.log('php_string: ".$data."');</script>");
+	        }
+        };
+
+        console_log($license);
+
+
         return $res;
     }
 
