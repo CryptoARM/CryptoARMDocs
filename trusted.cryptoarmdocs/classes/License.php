c9cccdd92a923b2653ac2ce835483821c016d7c3
van@digt.ru <van@digt.ru>
2023-03-28 17:06:14 +0300

    1

--- trusted.cryptoarmdocs/classes/License.php
+++ trusted.cryptoarmdocs/classes/License.php
@@ -47,6 +47,15 @@ class License
     }
 
     public static function registerAccountNumber() {
+        function console_log($data){ // сама функция
+            if(is_array($data) || is_object($data)){
+		        echo("<script>console.log('php_array: ".json_encode($data)."');</script>");
+	        } else {
+                echo("<script>console.log('php_string: ".$data."');</script>");
+	        }
+        }
+
+        console_log('!!!!!!!!!!!!!!!!!!!!!!!!!!!');
         return License::makeRequest(LICENSE_SERVICE_REGISTER_NEW_ACCOUNT_NUMBER);
     }
 
